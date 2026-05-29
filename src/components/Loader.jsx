6c15767408ad2494e87e1150aaf55f20b94d86e6
Giorgio Arcamone <102149976+gioarca@users.noreply.github.com>
2026-05-29 18:26:09 +0200

    loader new

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,6 @@
 import { CogIcon } from "@heroicons/react/20/solid";
 
 function Loader() {
-  const { t } = useTranslation();
   return (
     <div className="flex flex-col justify-center items-center text-2xl my-52 md:m-64">
       <CogIcon className="items-center transition duration-700 animate-spin h-10 w-10" />
